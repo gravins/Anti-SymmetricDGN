47ceef92650e67ec01a672f3e07de563ab49ddf4
Alessio Gravina <32573406+gravins@users.noreply.github.com>
2024-06-26 17:10:32 +0200

    minor fix

--- graph_prop_pred/run_all.sh
+++ graph_prop_pred/run_all.sh
@@ -7,12 +7,12 @@ data=GraphProp
 model= # specify here the model name
 
 task=dist
-nohup python3 -u main_bench.py --data_name $data --task $task --model_name $model --save_dir save_dir_GraphProp >out_$model\_$data\_$task 2>err_$model\_$data\_$task &
+nohup python3 -u main.py --data_name $data --task $task --model_name $model --save_dir save_dir_GraphProp >out_$model\_$data\_$task 2>err_$model\_$data\_$task &
 
 task=ecc
-nohup python3 -u main_bench.py --data_name $data --task $task --model_name $model --save_dir save_dir_GraphProp >out_$model\_$data\_$task 2>err_$model\_$data\_$task &
+nohup python3 -u main.py --data_name $data --task $task --model_name $model --save_dir save_dir_GraphProp >out_$model\_$data\_$task 2>err_$model\_$data\_$task &
 
 task=diam
-nohup python3 -u main_bench.py --data_name $data --task $task --model_name $model --save_dir save_dir_GraphProp >out_$model\_$data\_$task 2>err_$model\_$data\_$task &
+nohup python3 -u main.py --data_name $data --task $task --model_name $model --save_dir save_dir_GraphProp >out_$model\_$data\_$task 2>err_$model\_$data\_$task &
 
  
